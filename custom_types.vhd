bd91f2321b3bc9a4207d69d2bd6856f82be3661c
pedro.trindade <pedro.trindade@cnpem.br>
2025-07-29 10:48:40 -0300

    feat: new recursive sum tree

--- custom_types.vhd
+++ custom_types.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
-
 package custom_types is
-    type signed_array is array (integer range <>) of signed(15 downto 0);
+    constant N_WORD : natural := 16;
+    type signed_array is array (natural range <>) of signed(N_WORD-1 downto 0);
 end package custom_types;
